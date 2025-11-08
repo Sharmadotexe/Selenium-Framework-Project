f47e09e386c5685c640ca7f3b7163644eb7582fb
sharmadotexe <rockvikas300@gmail.com>
2025-11-08 17:40:56 +0530

    Fixed Concurrency problem

--- src/test/java/org/TestComponents/Listners.java
+++ src/test/java/org/TestComponents/Listners.java
@@ -14,20 +14,23 @@ import java.io.IOException;
 public class Listners extends BaseTest implements ITestListener {
     ExtentTest test;
     ExtentReports extent = ExtentReporterNG.getReportObject();
+    //Making Thread safe making sure each test has its own thread so they don't interrupt
+    ThreadLocal<ExtentTest> extentTest = new ThreadLocal<ExtentTest>();
 
     @Override
     public void onTestStart(ITestResult iTestResult) {
        test = extent.createTest(iTestResult.getMethod().getMethodName());
+       extentTest.set(test); // for this test it will assign a new thread id
     }
 
     @Override
     public void onTestSuccess(ITestResult iTestResult) {
-        test.log(Status.PASS, "Test is passed");
+        extentTest.get().log(Status.PASS, "Test is passed");
     }
 
     @Override
     public void onTestFailure(ITestResult iTestResult) {
-        test.fail(iTestResult.getThrowable());
+        extentTest.get().fail(iTestResult.getThrowable());
 
         try {
             driver = (WebDriver)iTestResult.getTestClass().getRealClass().getField("driver").get(iTestResult.getInstance());
@@ -41,7 +44,7 @@ public class Listners extends BaseTest implements ITestListener {
        } catch (IOException e) {
            throw new RuntimeException(e);
        }
-       test.addScreenCaptureFromPath(filePath, iTestResult.getMethod().getMethodName());
+        extentTest.get().addScreenCaptureFromPath(filePath, iTestResult.getMethod().getMethodName());
     }
 
     @Override
